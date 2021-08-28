83ea34c61d99f5387b7433df01aff471b1fe2f82
bakatsuyuki <bakatsuyuki@icloud.com>
2021-08-28 17:38:27 +0900

    First Commit.

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 大内克之 on 2021/08/28.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/ios_labApp.swift
@@ -0,0 +1,18 @@
+//
+//  ios_labApp.swift
+//  Shared
+//
+//  Created by 大内克之 on 2021/08/28.
+//
+//
+
+import SwiftUI
+
+@main
+struct ios_labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
